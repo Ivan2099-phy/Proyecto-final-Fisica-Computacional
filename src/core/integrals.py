1531be38e75b7de3e0ad050fd0def9bff4bd6d95
J Ivan MN <j.ivanmn2099@gmail.com>
2025-12-06 21:47:19 -0600

    feat(overlap_integral): agrega la función de solapamiento <x|x> y una base gaussiana_1s

--- src/core/integrals.py
+++ src/core/integrals.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy.integrate import simpson
 
+# Función para calcular integrales unidimensionales y tridimensionales con la regla de Simpson
 def simpson_1d (f, a, b, n=1000):
     """Calcula la integral de f en el intervalo [a, b] usando la regla de Simpson con n subintervalos."""
     x = np.linspace(a, b, n+1)
@@ -21,3 +22,23 @@ def simpson_3d(f, rmin, rmax, n=100):
     integral_xy = simpson(integral_x, y, axis=0)
     integral_xyz = simpson(integral_xy, z, axis=0)
     return integral_xyz
+
+# Bases gaussianas
+def gaussian_1s(x,y,z,alpha, Ax, Ay, Az):
+    """
+    Calcula el valor de una función base gaussiana para 1s en el punto r.
+    g(r) = exp(-alpha * |r - A|^2)
+    """
+    return np.exp(-alpha * ((x - Ax)**2 + (y - Ay)**2 + (z - Az)**2))
+
+# Integrales necesarias para Hartree-Fock
+
+# Integral de solapamiento S_ij = <chi_i | chi_j>
+def overlap_integral(alpha_i, Ai, alpha_j, Aj, rmin=-10, rmax=10, n=100):
+    """Calcula la integral de solapamiento entre dos funciones base."""
+    (Ax, Ay, Az), (Bx, By, Bz) = Ai,Aj  # Coordenadas de los centros de las funciones base
+    def integrand(X,Y,Z):
+        chi_i = gaussian_1s(X,Y,Z,alpha_i, Ax, Ay, Az)
+        chi_j = gaussian_1s(X,Y,Z,alpha_j, Bx, By, Bz)
+        return chi_i * chi_j
+    return simpson_3d(integrand, rmin=rmin, rmax=rmax, n=n)
