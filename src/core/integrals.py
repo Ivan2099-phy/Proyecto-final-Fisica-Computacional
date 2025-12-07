d0c715d36ff97319a78a02bc40527d20439ddef0
J Ivan MN <j.ivanmn2099@gmail.com>
2025-12-06 22:14:33 -0600

    feat(kinetic_integral): se añade el calculo de la integral de la energía cinética y el laplaciano para una función gaussiana

--- src/core/integrals.py
+++ src/core/integrals.py
@@ -31,7 +31,9 @@ def gaussian_1s(x,y,z,alpha, Ax, Ay, Az):
     """
     return np.exp(-alpha * ((x - Ax)**2 + (y - Ay)**2 + (z - Az)**2))
 
+#============================================================================
 # Integrales necesarias para Hartree-Fock
+#============================================================================
 
 # Integral de solapamiento S_ij = <chi_i | chi_j>
 def overlap_integral(alpha_i, Ai, alpha_j, Aj, rmin=-10, rmax=10, n=100):
@@ -41,4 +43,25 @@ def overlap_integral(alpha_i, Ai, alpha_j, Aj, rmin=-10, rmax=10, n=100):
         chi_i = gaussian_1s(X,Y,Z,alpha_i, Ax, Ay, Az)
         chi_j = gaussian_1s(X,Y,Z,alpha_j, Bx, By, Bz)
         return chi_i * chi_j
+    return simpson_3d(integrand, rmin=rmin, rmax=rmax, n=n)
+
+# Integral cinética T_ij = <chi_i | -1/2 ∇^2 | chi_j>
+# Laplaciano de la función base gaussiana 1s
+def laplacian_gaussian_1s(alpha_j, Aj, X, Y, Z):
+    """
+    Calcula el laplaciano de una función base gaussiana 1s.
+    Laplcaciano(exp(-apha* x**2))  = (4*alpha^2 * |r - A|^2 - 6*alpha) * g(r)
+    """
+    (Ax, Ay, Az) = Aj
+    r2 = (X - Ax)**2 + (Y - Ay)**2 + (Z - Az)**2
+    gj = gaussian_1s(X,Y,Z,alpha_j, Ax, Ay, Az)
+    return (4*alpha_j**2 * r2 - 6*alpha_j) * gj
+# Integral cinética
+def kinetic_integral(alpha_i, Ai, alpha_j, Aj, rmin=-10, rmax=10, n=100):
+    """Calcula la integral cinética entre dos funciones base."""
+    (Ax, Ay, Az), (Bx, By, Bz) = Ai,Aj  # Coordenadas de los centros de las funciones base
+    def integrand(X,Y,Z):
+        chi_i = gaussian_1s(X,Y,Z,alpha_i, Ax, Ay, Az)
+        lap_chi_j = laplacian_gaussian_1s(alpha_j, Aj, X, Y, Z)
+        return chi_i * (-0.5 * lap_chi_j)
     return simpson_3d(integrand, rmin=rmin, rmax=rmax, n=n)
