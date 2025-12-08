689ab7eaf0a4222350212860906ae097fcad7787
MMArely <marely@ciencias.unam.mx>
2025-12-08 03:39:09 -0600

    feat(basis_sets): Añadir funciones de normalización y solapamiento

--- src/core/basis_sets.py
+++ src/core/basis_sets.py
@@ -25,3 +25,19 @@ def N_gauss(alpha, l =[0,0,0]):
 
     """
     return (2 * alpha / math.pi) ** 0.75 * (((4*alpha)**(l[0]+l[1]+l[2]))/(factorial2(2*l[0] - 1) * factorial2(2*l[1] - 1) * factorial2(2*l[2] - 1))) ** 0.5
+
+def r_A_2(r, A):
+    """
+    Devuelve (r−A)^2
+    
+    Args:
+        r (list): Posición del electrón en coordenadas x, y, z.
+        A (list): Posición del centro de la gaussiana Ax, Ay, Az
+    """
+    return (r[0] - A[0])**2 + (r[1] - A[1])**2 + (r[2] - A[2])**2
+
+def overlapss(a, A, b, B):
+    """
+    Calcula el solapamiento entre dos gaussianas primitivas de tipo s.
+    """
+    return (np.pi/(a+b))**1.5 * np.exp(-a*b/(a+b)*r_A_2(A, B))
